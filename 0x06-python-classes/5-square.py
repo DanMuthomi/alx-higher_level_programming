b84b4ac0d833f1ee2666ea2a926ce64e8a52d835
DanMuthomi <thedanielmuthomi@gmail.com>
2023-01-24 17:24:47 +0000

    my_print changes

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -34,8 +34,7 @@ class Square:
     def my_prints(self):
         """Prints text to represent the square in # characters"""
         for i in range(0, self.__size):
-            for j in range(0, self.__size):
-                print("#", end="")
-            print()
-        if self.__size is 0:
-            print()
+            [print("#", end="") for j in range(self.__size)]
+            print("")
+        if self.__size == 0:
+            print("")
