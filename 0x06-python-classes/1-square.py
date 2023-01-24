92df9be6a04ec0463893039ff223580a4e1c0b0c
DanMuthomi <thedanielmuthomi@gmail.com>
2023-01-24 10:25:26 +0000

    define size attribute to square

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Define class square"""
+
+class Square:
+    """define square attributes"""
+
+    def __init__(self, size):
+        """Initialize a new square.
+
+        Args:
+            size (int): The size of the new square.
+        """
+        self.__size = size
