31835279e660115fef25f91eba957732d6409c26
DanMuthomi <thedanielmuthomi@gmail.com>
2023-01-24 10:42:06 +0000

    Size validation

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Define a class square"""
+
+class Square:
+    """define square attributes"""
+
+    def __init__(self, size=0):
+        """Initialize a new square.
+
+        Args:
+            size(int): this is the seize of the square.
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise VallueError("size must be >=0")
+        self.__size = size
