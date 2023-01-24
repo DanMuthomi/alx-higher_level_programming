5d7eeca13cf1b59b3f0547b7391eccb464bf69e0
DanMuthomi <thedanielmuthomi@gmail.com>
2023-01-24 16:47:59 +0000

    adding text to area output

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""Define square class"""
+
+
+class Square:
+    """define square attributes"""
+
+    def __init__(self, size=0):
+        """Initializr a new square.
+
+        Args:
+            size(int): this is the size of the square.
+        """
+        self.size = size
+
+    @property
+    def size(self):
+        """Get size of the square"""
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        """Set size of square"""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0"
+        self.__size = value
+
+    def area(self):
+        """Return are of square"""
+        return (self.__size * self.__size)
+
+    def my_prints(self):
+        """Prints text to represent the square in # characters"""
+        for row in range(0, self.__size):
+            for col in range(0, self.__size):
+                print("#", end="")
+            print()
+        if self.__size is 0:
+            print()
